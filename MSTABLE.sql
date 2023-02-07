40a177edd10869420ee128333de6676d8c31e07c
Ethan Gormley <egormley@redtms.com>
2023-02-07 07:02:45 -0500

    Add existing project files to Git

---
+++ MSTABLE.sql
@@ -0,0 +1,7 @@
+﻿create table Entry(
+	id int IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	OrderType VARCHAR(25) NOT NULL,
+	CustomerName VARCHAR(25) NOT NULL,
+	Date VARCHAR(25) NOT NULL,
+	CustomerName VARCHAR(25) NOT NULL
+);
---
+++ SQLQuery1.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE Actual_Orders (
+	id int IDENTITY(1,1) PRIMARY KEY,
+	OrderType VARCHAR(25) NOT NULL CHECK (OrderType IN('Standard', 'SaleOrder', 'PurchaseOrder', 'TransferOrder', 'ReturnOrder')),
+	CustomerName VARCHAR(50) NOT NULL,
+	Date varchar(25) NOT NULL,
+	CreatedbyUser VARCHAR(25) NOT NULL,
+);
